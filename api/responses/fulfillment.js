320a621d8f8d62ae2e64041b80feaea3f8a3f85b
👉dannis3434 <user@Users-MacBook-Pro.local>
2019-03-23 18:11:30 +0800

    Try get option from fb

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -226,12 +226,46 @@ agent.setFollowupEvent('looping')
 
     }
     async function price(agent) {
-        let params = agent.parameters;
-        price = params.price;
-        console.log("The price is " + price);
-        agent.setFollowupEvent('moveToBaseLine');;
+        var outputContexts = agent.context.get('outputcontexts');
+        console.log('outputContexts: ' + outputContexts);
+        var contextSurgery = outputContexts.parameters.surgery;
+        var surgery = await db.collection('surgery').doc(contextSurgery).get();
+
+        var abc = ['A', 'B', 'C', 'D', 'E', 'F','G','H','I','J', 'K','L','M' ,'N','O','P','Q','R','S', 'T','U','V','W','X','Y','Z'];
+        var count = 0;
+
+        async function getOptions() {
+            var output = await '此手術的基線案例為下:';
+            //Get all collections of "Specific" document
+            var optionsRef = await db.collection('surgery').doc(contextSurgery).collection('option').doc('specific').getCollections();
+            var generalOptionsRef = await db.collection('general').doc('option').getCollections();
+            //console.log(JSON.stringify(optionsRef));
+            //Use for each to loop all collections > element = a collection
+            for (const element of optionsRef) {
+                var tempElement = await element.doc('1').get(); //First doc of each collection is the base case
+                console.log("Specific內容是: " + tempElement.data()['content']);
+                output += await abc[count] + tempElement.data()['content'] + ",  ";
+                //output += await abc[count] + tempElement.data()['內容'] + ",  ";
+                count++;
+                console.log(output);
+            }
+            for (const generalElement of generalOptionsRef) {
+                var tempElement = await generalElement.doc('1').get(); //First doc of each collection is the base case
+                console.log("General內容是:"  + tempElement.data()['content']);
+               // console.log("----" + tempElement.data().title);
+                output += await abc[count] + tempElement.data()['content'] + ",  ";
+                //output += await abc[count] + tempElement.data()['內容'] + ",  ";
+                count++;
+                console.log(output);
+            }
+
+            return output+"                    ";
+        }
+        var outputMessage =await getOptions()+"如你的個案付合以上條件，請回覆[ok].否則請按A-F更改選項,或按[X]告訴我們你個案與一般手術不同的地方。";
+        agent.add(outputMessage);
 
     }
+
     async function followup(agent){
 
     }
